03b93e67e64fbcf7b9bacfb2325f5645236f827f
rubenjoy <rubenjoy@gmail.com>
2017-02-01 15:27:18 +0800

    Revert "Revert "add License""
    This reverts commit f50e8facd4742a54d04d9d4fa9bec9277b5e9f3a.

---
+++ src/components/todo/TodoItem.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {partial} from '../../lib/util'
+
+export const TodoItem = (props) => {
+	const handleToggle = partial(props.handleToggle,
+		props.id)
+	const handleRemove = partial(props.handleRemove, props.id)
+	return (
+		<li>
+		<span className="delete-item">
+			<a href="#"
+			onClick={handleRemove}>X</a>
+		</span>
+		<input type="checkbox" 
+		checked={props.isComplete}
+		onChange={handleToggle}
+		/>
+		{props.name}
+		</li>
+	);
+}
+
+TodoItem.propTypes = {
+	name: React.PropTypes.string.isRequired,
+	isComplete: React.PropTypes.bool,
+	id: React.PropTypes.number.isRequired
+}
